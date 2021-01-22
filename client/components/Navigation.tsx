f7f4b08ad2ba3bc238bddc97cbef75e7c9e37024
Dawid Dahl <dawiddahl@gmail.com>
2021-01-22 23:26:37 +0100

    worked on navbar

--- client/components/Navigation.tsx
+++ client/components/Navigation.tsx
@@ -1,19 +1,96 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 
 type Props = {};
 
-const Navigation: React.FC<Props> = () => (
-	<Wrapper>
-		<Shell></Shell>
-	</Wrapper>
-);
+const Navigation: React.FC<Props> = () => {
+	const [isNavToggled, setIsNavToggled] = useState(false);
+
+	const handleClick = () => setIsNavToggled(!isNavToggled);
+
+	return (
+		<Wrapper>
+			<Shell>
+				<NavButton onClick={handleClick} isNavToggled={isNavToggled} />
+				<Bar isNavToggled={isNavToggled}>
+					<List>
+						<Icon
+							imageLink="https://s2.svgbox.net/hero-outline.svg?color=7a69ff&ic=identification"
+							isNavToggled={isNavToggled}
+						/>
+						<Icon
+							imageLink="https://s2.svgbox.net/hero-outline.svg?color=7a69ff&ic=archive"
+							isNavToggled={isNavToggled}
+						/>
+						<Icon
+							imageLink="https://s2.svgbox.net/hero-outline.svg?color=7a69ff&ic=badge-check"
+							isNavToggled={isNavToggled}
+						/>
+						<Icon
+							imageLink="https://s2.svgbox.net/hero-outline.svg?color=7a69ff&ic=share"
+							isNavToggled={isNavToggled}
+						/>
+					</List>
+				</Bar>
+			</Shell>
+		</Wrapper>
+	);
+};
+
+type StyledNavButtonProps = {
+	isNavToggled: boolean;
+};
+
+const NavButton = styled.div<StyledNavButtonProps>`
+	position: absolute;
+	height: 100%;
+	width: 3.5em;
+	border-radius: ${props => (props.isNavToggled ? "0 0" : "50%")};
+	background-color: ${props => (props.isNavToggled ? "var(--main-color)" : "rgb(176 167 255)")};
+	z-index: 2;
+	background-image: url("//s.svgbox.net/hero-outline.svg?fill=fff#menu-alt-1");
+	background-size: 50%;
+	background-repeat: no-repeat;
+	background-position: center;
+	transition: all 0.3s;
+	cursor: pointer;
+
+	@media only screen and (max-width: 474px) {
+	}
+`;
+
+type StyledBarProps = {
+	isNavToggled: boolean;
+};
+
+const Bar = styled.div<StyledBarProps>`
+	position: absolute;
+	height: 100%;
+	width: ${props => (props.isNavToggled ? "100%" : "0%")};
+	transition: all 0.4s ease-in-out 0.1s;
+	clip-path: ${props =>
+		props.isNavToggled ? "ellipse(20em 3em at 50% 50%)" : "ellipse(2.25em 2.25em at 0% 50%)"};
+	background: linear-gradient(
+		to right bottom,
+		rgba(255, 255, 255, 0.7),
+		rgba(255, 255, 255, 0.3)
+	);
+	z-index: 1;
+	border-radius: 2em;
+	left: 5px;
+
+	@media only screen and (max-width: 474px) {
+		width: ${props => (props.isNavToggled ? "100%" : "0%")};
+	}
+`;
 
 const Shell = styled.div`
-	height: 4.5em;
+	position: relative;
+	height: 3.5em;
 	width: 20em;
-	background-color: red;
-	z-index: 1;
+	margin: 0 1em;
+	border-radius: 2em;
+	overflow: hidden;
 
 	@media only screen and (max-width: 474px) {
 		width: 18em;
@@ -31,6 +108,41 @@ const Wrapper = styled.div`
 	bottom: 0;
 
 	@media only screen and (max-width: 474px) {
+	}
+`;
+
+const List = styled.ul`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	list-style-type: none;
+	height: 100%;
+	margin: 0;
+`;
+
+type StyledIconProps = {
+	imageLink: string;
+	isNavToggled: boolean;
+};
+
+const Icon = styled.li<StyledIconProps>`
+	height: 100%;
+	width: 3.5em;
+	background-image: ${props => `url(${props.imageLink})`};
+	background-size: 50%;
+	background-repeat: no-repeat;
+	background-position: center;
+	z-index: 2;
+	cursor: pointer;
+	opacity: ${props => (props.isNavToggled ? "100%" : "0%")};
+
+	&:hover {
+		background-color: rgba(255, 255, 255, 0.2);
+		border-radius: 50%;
+	}
+
+	@media only screen and (max-width: 474px) {
+		background-size: 40%;
 	}
 `;
 

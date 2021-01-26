1de85c95ecea39e229c6bd2359aabd8b5874eb4f
Dawid Dahl <dawiddahl@gmail.com>
2021-01-26 22:49:14 +0100

    worked on Home component

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,13 +1,95 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styled from "styled-components";
-import Glass from "./Glass";
 
 const Home: React.FC = () => {
+	const imageUrl = "https://bit.ly/3a41zHj";
+
 	return (
-		<>
-			<p>Hello!</p>
-		</>
+		<Wrapper>
+			<ImageArea>
+				<Image href={imageUrl} target="_blank">
+					<img src={imageUrl} alt="profile picture" />
+				</Image>
+				<Header>
+					<h1>Dawid Dahl</h1>
+					<h3>Full-Stack Web Developer</h3>
+					<p>Stockholm</p>
+				</Header>
+			</ImageArea>
+			<TextArea>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi in assumenda
+					magnam impedit ea, ex quam veniam adipisci omnis fuga doloremque placeat aliquid
+					quis mollitia eum iure amet quidem. Vitae.
+				</p>
+			</TextArea>
+		</Wrapper>
 	);
 };
+
+const Wrapper = styled.div`
+	position: relative;
+	height: 100%;
+	width: 100%;
+	border-radius: inherit;
+	overflow: hidden;
+
+	@media only screen and (max-width: 474px) {
+	}
+`;
+
+const ImageArea = styled.div`
+	height: 60%;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
+
+const Image = styled.a`
+	border-radius: 50%;
+	width: 10em;
+	height: 10em;
+	box-shadow: rgb(50, 50, 93) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+	cursor: pointer;
+
+	> img {
+		width: 10em;
+		height: 10em;
+		border-radius: 50%;
+		object-fit: cover;
+		object-position: 0px -25px;
+	}
+`;
+
+const Header = styled.div`
+	color: var(--white-transparent);
+
+	h1 {
+		margin-top: 1em;
+	}
+
+	h3 {
+		margin: 0;
+	}
+
+	p {
+		margin: 0;
+	}
+`;
+
+const TextArea = styled.div`
+	height: 40%;
+	background: linear-gradient(
+		to right bottom,
+		rgba(255, 255, 255, 0.7),
+		rgba(255, 255, 255, 0.3)
+	);
+	border-radius: 15rem 0rem 2rem 2rem;
+	border-top: 10px solid var(--main-color-light);
+	color: var(--main-color);
+	padding: 5em 3em 0 6em;
+`;
 
 export default Home;
--- client/components/Main.tsx
+++ client/components/Main.tsx
@@ -1,35 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 import {useLocation} from "react-router";
 import {animated, useTransition, config} from "react-spring";
-import styled from "styled-components";
-import {IconsList} from "../content/Icons";
 import Glass from "./Glass";
 import Routes from "./Routes";
 
 export const Main = () => {
 	const location = useLocation();
-
-	const currentPathIndex: number = (IconsList[
-		location.pathname.slice(1) as any
-	] as unknown) as number;
-
-	const [prevLocationIndex, setPrevLocationIndex] = useState(currentPathIndex);
-
-	const transitionToRight = useTransition(location, location => location.pathname, {
-		config: config.gentle,
-		from: {
-			opacity: 0,
-			transform: "translateX(-100vw)",
-			position: "absolute",
-			backdropFilter: "blur(0.5em)",
-		},
-		enter: {opacity: 1, transform: "translateX(0vw)"},
-		leave: {opacity: 0, transform: "translateX(110vw)"},
-		//@ts-ignore
-		onRest: () => {
-			setPrevLocationIndex(currentPathIndex);
-		},
-	});
 
 	const transitionToLeft = useTransition(location, location => location.pathname, {
 		config: config.gentle,
@@ -41,33 +17,19 @@ export const Main = () => {
 		},
 		enter: {opacity: 1, transform: "translateX(0vw)"},
 		leave: {opacity: 0, transform: "translateX(-110vw)"},
-		//@ts-ignore
-		onRest: () => {
-			setPrevLocationIndex(currentPathIndex);
-		},
 	});
 
 	return (
 		<>
-			{currentPathIndex >= prevLocationIndex
-				? transitionToLeft.map(({item, key, props: transitionProps}) => (
-						<animated.div key={key} style={transitionProps}>
-							<Glass>
-								<Routes location={item} />
-							</Glass>
-						</animated.div>
-				  ))
-				: transitionToRight.map(({item, key, props: transitionProps}) => (
-						<animated.div key={key} style={transitionProps}>
-							<Glass>
-								<Routes location={item} />
-							</Glass>
-						</animated.div>
-				  ))}
+			{transitionToLeft.map(({item, key, props: transitionProps}) => (
+				<animated.div key={key} style={transitionProps}>
+					<Glass>
+						<Routes location={item} />
+					</Glass>
+				</animated.div>
+			))}
 		</>
 	);
 };
-
-const Wrapper = styled.div``;
 
 export default Main;
